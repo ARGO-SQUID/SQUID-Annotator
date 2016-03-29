25269eb9b11d8a8c3a7d5b5efa8179ccf7d561c8
Andrew Glass <andrewglass1@gmail.com>
2016-03-29 17:11:22 -0400

    keypress works with annotation

--- app/assets/javascripts/keypress.js
+++ app/assets/javascripts/keypress.js
@@ -1,40 +1,56 @@
 keypress = {
 
   init: function(){
-    // $(document).keydown(function(e) {
-    //   e.preventDefault();
-    //   keypress.routeKeypress(e.which);
-    //   return false
-    // });
+    $(document).keydown(function(e) {
+      result = keypress.routeKeypress(e.which);
+      if(result == true){
+        e.preventDefault();
+        return false
+      }
+    });
   },
 
   routeKeypress: function(keypressId){
     if(streetView.inStreetViewMode() && !annotate.annotationViewMode()){
-      keypress.streetViewKeypress(keypressId)
+      return keypress.streetViewKeypress(keypressId)
     }
     else if(annotate.annotationViewMode()){
-      keypress.annotationKeypress(keypressId)
+      return keypress.annotationKeypress(keypressId)
+    }
+    else{
+      return false
     }
   },
 
   streetViewKeypress: function(keypressId){
     if(keypressId == 39){
       streetView.moveCarouselForward()
+      return true
     }
     else if(keypressId == 37){
       streetView.moveCarouselBackward()
+      return true
     }
     else if(keypressId == 27){
       streetView.closeStreetView()
+      return true
     }
     else if(keypressId == 13){
       annotate.launchForLocation(selectedLocationMarker.location);
+      return true
+    }
+    else{
+      return false
     }
   },
 
   annotationKeypress: function(keypressId){
     if(keypressId == 27){
       annotate.closeAnnotationView()
+      return true
+    }
+    else{
+      return false
     }
   }
 
