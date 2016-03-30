b2068cb608b89c4a294cf39923b38375a69c9996
Andrew Glass <andrewglass1@gmail.com>
2016-03-30 17:17:02 -0400

    more styles

--- app/assets/javascripts/keypress.js
+++ app/assets/javascripts/keypress.js
@@ -25,10 +25,18 @@ keypress = {
   streetViewKeypress: function(keypressId){
     if(keypressId == 39){
       streetView.moveCarouselForward()
+      $('#carousel-right').addClass('hover-state')
+      setTimeout(function(){
+        $('#carousel-right').removeClass('hover-state')
+      }, 100)
       return true
     }
     else if(keypressId == 37){
       streetView.moveCarouselBackward()
+      $('#carousel-left').addClass('hover-state')
+      setTimeout(function(){
+        $('#carousel-left').removeClass('hover-state')
+      }, 100)
       return true
     }
     else if(keypressId == 27){
@@ -36,7 +44,11 @@ keypress = {
       return true
     }
     else if(keypressId == 32){
+      $('#launch-annotate').addClass('hover-state')
       annotate.launchForLocation(selectedLocationMarker.location);
+      setTimeout(function(){
+        $('#launch-annotate').removeClass('hover-state')
+      }, 100)
       return true
     }
     else{
