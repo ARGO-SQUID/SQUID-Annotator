4345d920c94d39c735ad5ab3340b2db54a2325d2
Andrew Glass <andrewglass1@gmail.com>
2016-04-05 12:17:55 -0400

    remove httpbasic auth

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,7 +3,6 @@ class ApiController < ApplicationController
   WHITELIST = ["http://arcane-brushlands-86807.herokuapp.com/"]
 
   before_action :ensure_origin_whitelisted
-  skip_before_action :user_basic_auth
 
   def ensure_origin_whitelisted
     return if Rails.env == "development"
