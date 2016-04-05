5752f43ee195cb93f4c423d5ee02c8453dc6e2ae
Andrew Glass <andrewglass1@gmail.com>
2016-04-05 16:36:30 -0400

    default scope locations in order

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,6 +4,8 @@ class Location < ActiveRecord::Base
 
   validates_presence_of :street_id, :lat, :long
 
+  default_scope { order(order: :asc) }
+
   scope :light, -> { select('lat, long') }
   scope :medium, -> { select("id, lat, long, bumpiness, image, to_char(reading_timestamp, 'Mon DD HH24:MI:SS') as display_reading_timestamp") }
   scope :with_annotations, -> { where("annotation_count > ?", 0) }
