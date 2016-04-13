625e4278cdef8c686db449a10fcabd44dfe1dfd2
Andrew Glass <andrewglass1@gmail.com>
2016-04-13 11:00:25 -0400

    add sub-urls to api whitelist

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,10 @@
 class ApiController < ApplicationController
 
-  WHITELIST = ["http://arcane-brushlands-86807.herokuapp.com/", "http://squidannotator.herokuapp.com/"]
+  WHITELIST = ["http://arcane-brushlands-86807.herokuapp.com/",
+               "http://squidannotator.herokuapp.com/",
+               "http://squidannotator.herokuapp.com/syracuse",
+               "http://squidannotator.herokuapp.com/hoboken",
+               "http://squidannotator.herokuapp.com/staten_island"]
 
   before_action :ensure_origin_whitelisted
 
